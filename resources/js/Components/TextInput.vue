192e32ad894503e2521561ab2decdd62c50b91c1
ardavanshamroshan <ardavan2000.ashr@gmail.com>
2023-01-09 20:08:40 +0330

    required package installed #2

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 
-defineProps(['modelValue']);
+defineProps({
+    modelValue: String,
+});
 
 defineEmits(['update:modelValue']);
 
@@ -18,9 +20,9 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
     <input
-        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        ref="input"
+        class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
-        ref="input"
-    />
+    >
 </template>
